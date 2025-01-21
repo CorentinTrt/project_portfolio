b3453cec458bbe16e946c5d5ffec46dd5462c5d3
CorentinTrt <corentintruffaut@gmail.com>
2025-01-21 16:47:24 -0500

    chore: update react, react-dom, next, react-simple-icons

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  output: 'standalone',
   sassOptions: {
     additionalData: `@use "styles/utils/animations.scss" as A; @use "styles/utils/variables.scss" as V;`,
-    excludePaths: "/styles"
+    excludePaths: '/styles',
   },
 };
 
--- src/components/3_shared/icons/Icons.tsx
+++ src/components/3_shared/icons/Icons.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 import {
-  Android,
-  Craftcms,
-  Gatsby,
-  Graphql,
-  Ios,
-  Mongodb,
-  Mysql,
-  Nodedotjs,
-  ReactJs,
-  Threedotjs,
-  Typescript,
+  SiAndroid,
+  // Craftcms,
+  // Gatsby,
+  // GoogleCloud,
+  // Graphql,
+  // Ios,
+  // Mongodb,
+  // Mysql,
+  // Nodedotjs,
+  // Nx,
+  // ReactJs,
+  // Threedotjs,
+  // Typescript,
 } from '@icons-pack/react-simple-icons';
 
 type Props = {
@@ -24,32 +26,36 @@ const Icon = (props: Props) => {
   const iconProps = {
     width: '3rem',
     height: '3rem',
-    color: ' #0A0A0A',
+    color: '#0A0A0A',
   };
 
   switch (label) {
     case 'android':
-      return <Android {...iconProps} />;
-    case 'craft':
-      return <Craftcms {...iconProps} />;
-    case 'gatsby':
-      return <Gatsby {...iconProps} />;
-    case 'graphql':
-      return <Graphql {...iconProps} />;
-    case 'ios':
-      return <Ios {...iconProps} />;
-    case 'mongodb':
-      return <Mongodb {...iconProps} />;
-    case 'mysql':
-      return <Mysql {...iconProps} />;
-    case 'node.js':
-      return <Nodedotjs {...iconProps} />;
-    case 'react':
-      return <ReactJs {...iconProps} />;
-    case 'three.js':
-      return <Threedotjs {...iconProps} />;
-    case 'typescript':
-      return <Typescript {...iconProps} />;
+      return <SiAndroid {...iconProps} />;
+   // case 'craft':
+   //   return <Craftcms {...iconProps} />;
+   // case 'gatsby':
+   //   return <Gatsby {...iconProps} />;
+   // case 'google clous':
+   //   return <Gatsby {...iconProps} />;
+   // case 'graphql':
+   //   return <Graphql {...iconProps} />;
+   // case 'ios':
+   //   return <Ios {...iconProps} />;
+   // case 'mongodb':
+   //   return <Mongodb {...iconProps} />;
+   // case 'mysql':
+   //   return <Mysql {...iconProps} />;
+   // case 'node.js':
+   //   return <Nodedotjs {...iconProps} />;
+   // case 'nx':
+   //   return <Nx {...iconProps} />;
+   // case 'react':
+   //   return <ReactJs {...iconProps} />;
+   // case 'three.js':
+   //   return <Threedotjs {...iconProps} />;
+   // case 'typescript':
+   //   return <Typescript {...iconProps} />;
     default:
       return <></>;
   }
